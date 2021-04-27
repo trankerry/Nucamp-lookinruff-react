2f433e890a7c2890d0f436a6159a035ab109e22a
Kerry <trankerry94@gmail.com>
2021-04-26 22:02:40 -0700

    added jumbotron

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label } from 'reactstrap';
+// import { NavLink } from 'react-router-dom';
+
+class Header extends Component {
+    render() { 
+        return (
+            <>
+                <Jumbotron fluid>
+                    <div className="container">
+                        <div className='row'>
+                            <div className='col py-5'>
+                                <h1>Hello</h1>
+                                <h2>helloworld</h2>
+                            </div>
+                        </div>
+                    </div>
+                </Jumbotron>
+                <Navbar dark sticky='top'>
+    
+                </Navbar>
+            </>
+        );
+    }
+}
+ 
+export default Header;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from './HeaderComponent';
+// import Footer from './FooterComponent';
+// import Home from './HomeComponent';
+// import Contact from './ContactComponent';
+// import About from './AboutComponent';
+
+class Main extends Component {
+    render() { 
+        return ( 
+            <div>
+                <Header />
+            </div>
+         );
+    }
+}
+ 
+export default Main;
